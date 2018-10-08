a34f250a8acc21255628219343a7f6b6425d5a2b
Leopold Chmielewski <leopold.chmielewski@gmail.com>
2018-10-08 16:11:46 +0100

    Code cleaning

--- services/users/project/api/auth.py
+++ services/users/project/api/auth.py
@@ -8,6 +8,7 @@ from project import db, bcrypt
 
 auth_blueprint = Blueprint('auth', __name__)
 
+
 @auth_blueprint.route('/auth/register', methods=['POST'])
 def register_user():
     # get post data
@@ -48,6 +49,7 @@ def register_user():
         db.session.rollback()
         return jsonify(response_object), 400
 
+
 @auth_blueprint.route('/auth/login', methods=['POST'])
 def login_user():
     # get post data
@@ -77,6 +79,7 @@ def login_user():
         response_object['message'] = 'Try again.'
         return jsonify(response_object), 500
 
+
 @auth_blueprint.route('/auth/logout', methods=['GET'])
 def logout_user():
     # get auth token
@@ -97,6 +100,7 @@ def logout_user():
             return jsonify(response_object), 401
     else:
         return jsonify(response_object), 403
+
 
 @auth_blueprint.route('/auth/status', methods=['GET'])
 def get_user_status():
--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -3,8 +3,8 @@
 
 import json
 
-from project import db
-from project.api.models import User
+# from project import db
+# from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
@@ -250,4 +250,3 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(
                 data['message'] == 'Invalid token. Please log in again.')
             self.assertEqual(response.status_code, 401)
-            

6b8c67fa029cd52c619b641427e7e3b64e84296a
Leopold Chmielewski <leopold.chmielewski@gmail.com>
2018-10-10 16:36:27 +0100

    ...again...

--- services/client/src/components/forms/Form.jsx
+++ services/client/src/components/forms/Form.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
+import { registerFormRules, loginFormRules } from '.form-rules.js';
+import FormError from './FormError.jsx';
+
 class Form extends Component {
   constructor (props) {
     super(props);
@@ -10,6 +13,11 @@ class Form extends Component {
         username: '',
         email: '',
         password: ''
+      },
+      registerFormRules: registerFormRules,
+      loginFormRules: loginFormRules,
+      formRules: '',
+      valid: false,
       }
     };
     this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
@@ -17,10 +25,12 @@ class Form extends Component {
   };
   componentDidMount() {
     this.clearForm();
+    this.validateForm();
   };
   componentWillReceiveProps(nextProps) {
     if (this.props.formType !== nextProps.formType) {
       this.clearForm();
+      this.validateForm();
     };
   };
   clearForm() {
@@ -49,17 +59,90 @@ class Form extends Component {
       this.clearForm();
       this.props.loginUser(res.data.auth_token);
     })
-    .catch((err) => { console.log(err); });
+    .catch((err) => { .catch((err) => {
+      // new
+      if (formType === 'Login') {
+        this.props.createMessage('User does not exist.', 'danger');
+      };
+      // new
+      if (formType === 'Register') {
+        this.props.createMessage('That user already exists.', 'danger');
+      };
+    });
+  };
+  validateForm() {
+    // define self as this
+    const self = this;
+    // get form data
+    const formData = this.state.formData;
+    // reset all rules
+    self.resetRules()
+    // validate register form
+    if (self.props.formType === 'Register') {
+      const formRules = self.state.registerFormRules;
+      if (formData.username.length > 5) formRules[0].valid = true;
+      if (formData.email.length > 5) formRules[1].valid = true;
+      if (this.validateEmail(formData.email)) formRules[2].valid = true;
+      if (formData.password.length > 10) formRules[3].valid = true;
+      self.setState({registerFormRules: formRules})
+      if (self.allTrue()) self.setState({valid: true});
+    }
+    // validate login form
+    if (self.props.formType === 'Login') {
+      const formRules = self.state.loginFormRules;
+      if (formData.email.length > 0) formRules[0].valid = true;
+      if (formData.password.length > 0) formRules[1].valid = true;
+      self.setState({loginFormRules: formRules})
+      if (self.allTrue()) self.setState({valid: true});
+    }
+  };
+  allTrue() {
+    let formRules = loginFormRules;
+    if (this.props.formType === 'Register') {
+      formRules = registerFormRules;
+    }
+    for (const rule of formRules) {
+      if (!rule.valid) return false;
+    }
+    return true;
+  };
+  resetRules() {
+    const registerFormRules = this.state.registerFormRules;
+    for (const rule of registerFormRules) {
+      rule.valid = false;
+    }
+    this.setState({registerFormRules: registerFormRules})
+    const loginFormRules = this.state.loginFormRules;
+    for (const rule of loginFormRules) {
+      rule.valid = false;
+    }
+    this.setState({loginFormRules: loginFormRules})
+    this.setState({valid: false});
+  };
+  validateEmail(email) {
+    // eslint-disable-next-line
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+});
   };
   render() {
     if (this.props.isAuthenticated) {
       return <Redirect to='/' />;
     };
+    let formRules = this.state.loginFormRules;  // new
+    // new
+    if (this.props.formType === 'Register') {
+      formRules = this.state.registerFormRules;
+    }
     return (
       <div>
         <h1 className="title is-1">{this.props.formType}</h1>
         <hr/><br/>
-        <form onSubmit={(event) => this.props.handleUserFormSubmit(event)}>
+          <FormErrors
+            formType={this.props.formType}
+            formRules={formRules}
+          />
+        <form onSubmit={(event) => this.handleUserFormSubmit(event)}>
           {this.props.formType === 'Register' &&
             <div className="field">
               <input
@@ -69,7 +152,7 @@ class Form extends Component {
                 placeholder="Enter a username"
                 required
                 value={this.state.formData.username}
-                onChange={this.props.handleFormChange}
+                onChange={this.handleFormChange}
               />
             </div>
           }
@@ -81,7 +164,7 @@ class Form extends Component {
               placeholder="Enter an email address"
               required
               value={this.state.formData.email}
-              onChange={this.props.handleFormChange}
+              onChange={this.handleFormChange}
             />
           </div>
           <div className="field">
@@ -92,13 +175,14 @@ class Form extends Component {
               placeholder="Enter a password"
               required
               value={this.state.formData.password}
-              onChange={this.props.handleFormChange}
+              onChange={this.handleFormChange}
             />
           </div>
           <input
             type="submit"
             className="button is-primary is-medium is-fullwidth"
             value="Submit"
+            disabled={!this.state.valid}
           />
         </form>
       </div>
